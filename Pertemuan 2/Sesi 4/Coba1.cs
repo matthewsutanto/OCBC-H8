04ca66dac0a5e2112075e1a9ec084d4beedcb181
matthewsutanto <41150197+matthewsutanto@users.noreply.github.com>
2021-10-12 13:22:58 +0700

    new coba

---
+++ Pertemuan 2/Sesi 4/Coba1.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Coba1
+{
+    public static void Main(string[] args)
+    {   
+        Console.Write("Jumlah data yang mau diinput : ");
+        int jumlah = int.Parse(Console.ReadLine());
+        string[] contoh = new string[jumlah];
+        
+        int i=0;
+        int ke=0;
+        for(i=0;i<jumlah;i++) {
+            ke = i+1;
+            Console.Write("Data ke-"+ke+"=");
+            contoh[i]=Console.ReadLine();
+        }
+
+        Console.WriteLine("Menampilkan semua data dalam array");
+        Console.WriteLine("");
+
+        foreach (string item in contoh)
+        {
+            Console.WriteLine("Item : "+item);
+        }
+    }
+}
