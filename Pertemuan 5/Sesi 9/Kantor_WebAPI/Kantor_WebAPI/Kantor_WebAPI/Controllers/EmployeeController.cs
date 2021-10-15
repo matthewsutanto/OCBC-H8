cc4e0ab3b686b12ac4976c90250f3c0cd0350b9e
matthewsutanto <41150197+matthewsutanto@users.noreply.github.com>
2021-10-15 11:37:16 +0700

    pertemuan 9

---
+++ Pertemuan 5/Sesi 9/Kantor_WebAPI/Kantor_WebAPI/Kantor_WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using Kantor_WebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kantor_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : Controller
+    {
+        private EmployeeContext _context;
+
+        public EmployeeController(EmployeeContext context)
+        {
+            this._context = context;
+        }
+
+        public ActionResult<IEnumerable<EmployeeItem>> GetEmployeeItems()
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(EmployeeContext)) as EmployeeContext;
+
+            return _context.GetAllEmployee();
+        }
+        [HttpGet("{id}", Name = "Get")]
+        public ActionResult<IEnumerable<EmployeeItem>> GetEmployeeItem(String id)
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(EmployeeContext)) as EmployeeContext;
+            return _context.GetEmployee(id);
+        }
+    }
+}
